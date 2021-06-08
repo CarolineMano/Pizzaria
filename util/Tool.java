dd6648319a9578e5d0a91fd14dbfb37679f1d893
CarolineMano <carol.marinho.mano@gmail.com>
2021-06-07 22:29:37 -0300

    Create Tool.java
    Criação de classe auxiliar para converter entradas de string para int

---
+++ util/Tool.java
@@ -0,0 +1,22 @@
+package util;
+import java.util.Scanner;
+
+public class Tool {
+    
+    public static int convertStringToInt(Scanner input) {
+        boolean continueLoop = true;
+        String optionAsString;
+        int optionAsInt = 0;
+        do {
+            optionAsString = input.nextLine();
+            try {
+                optionAsInt = Integer.parseInt(optionAsString);
+                continueLoop = false;
+            } catch (Exception e) {
+                System.out.println("Você não digitou um número! Digite novamente.");
+                continueLoop = true;
+            }
+        } while (continueLoop);
+        return optionAsInt;
+    }
+}

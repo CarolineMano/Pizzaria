1694c96a16b99ca24dc48d8c0d6f53107dd4f94a
CarolineMano <carol.marinho.mano@gmail.com>
2021-06-07 21:14:24 -0300

    Create models
    Criação de duas classes, uma com a definição de produto e outra com o menu da pizzaria

---
+++ model/Menu.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Menu {
+    private Product[] menu;
+
+    public Menu() {
+        menu = new Product [] {
+                new Product("Mussarela", 24.99),
+                new Product("Quatro queijos", 26.99),
+                new Product("Pepperoni", 28.99),
+                new Product("Frango com catupiry", 26.99)
+        };
+    }
+
+    public String showAllToppings() {
+        String output = "***Menu***\n";
+        for (int i = 0; i < menu.length; i++) {
+            output += i+1 + "-" + menu[i].getName() + " - R$" + menu[i].getPrice() + "\n";
+        }
+        return output;
+    }
+}
